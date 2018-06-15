a19ac6fb10a19d3965116ec3c0e8165980725e3a
OverAhah <kozak.art@gmail.com>
2018-06-15 13:51:24 +0300

    Estb mnogo Chego

--- app/src/main/java/com/example/user/game_project/Main4Activity.kt
+++ app/src/main/java/com/example/user/game_project/Main4Activity.kt
@@ -3,20 +3,19 @@ package com.example.user.game_project
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main3.*
+import kotlinx.android.synthetic.main.activity_main4.*
 
-class Main2Activity : AppCompatActivity() {
+class Main4Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main4)
 
-        Exit.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        Exit4_1.setOnClickListener {
+            val intent = Intent(this, Main3Activity::class.java)
             startActivity(intent)
         }
     }
 }
-
