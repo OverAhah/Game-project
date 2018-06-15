ffee1690ebf3437cf274aa413fba3b944e78957e
OverAhah <kozak.art@gmail.com>
2018-06-15 12:34:40 +0300

    1

---
+++ app/src/main/java/com/example/user/game_project/Main2Activity.kt
@@ -0,0 +1,16 @@
+package com.example.user.game_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+    }
+}
+
