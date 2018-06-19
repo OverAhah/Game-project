1913994b046fbcd0a453e329ffb7b84781d6947c
Dimentr01 <dima.zaharcev@mail.ru>
2018-06-19 12:05:40 +0300

    Test7

---
+++ app/src/main/java/com/example/user/myapplication/Main4Activity.kt
@@ -0,0 +1,22 @@
+package com.example.user.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main3.*
+import kotlinx.android.synthetic.main.activity_main4.*
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+
+        Exit41.setOnClickListener {
+            val intent = Intent(this, Main3Activity::class.java)
+            startActivity(intent)
+        }
+
+        val count = 12
+    }
+}
