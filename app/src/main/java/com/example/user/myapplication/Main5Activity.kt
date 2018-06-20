c7559d2b2d1b3578e641c617dcc48c21849ce219
Dimentr01 <dima.zaharcev@mail.ru>
2018-06-20 13:12:38 +0300

    Demo (Stupid Demo)

--- app/src/main/java/com/example/user/myapplication/Main5Activity.kt
+++ app/src/main/java/com/example/user/myapplication/Main5Activity.kt
@@ -1,8 +1,10 @@
 package com.example.user.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main5.*
+import kotlinx.android.synthetic.main.activity_main6.*
 
 class Main5Activity : AppCompatActivity() {
 
@@ -13,6 +15,9 @@ class Main5Activity : AppCompatActivity() {
         Text1.text = "К сожалению, вы умерли по пути в столицу." +
                 "Чтож, повезет в другой раз"
 
-
+        Restart1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
